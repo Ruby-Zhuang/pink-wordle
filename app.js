435f305c55a4580b20661cedf48a3fb178d13a4d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-02-22 21:00:40 -0500

    Remove message after 2 seconds

--- app.js
+++ app.js
@@ -155,4 +155,7 @@ const showMessage = (message) => {
   const messageElement = document.createElement('p');
   messageElement.textContent = message;
   messageDisplay.append(messageElement);
+
+  // Remove message after 2 seconds
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
 };
