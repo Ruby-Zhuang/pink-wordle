f9a4cfa80fa8c4e4a7459795ea8e311797ffa51e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-02-22 21:49:01 -0500

    Remove app.use for now

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const axios = require('axios').default;
 const express = require('express');
 const app = express();
 
-app.use();
-
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
