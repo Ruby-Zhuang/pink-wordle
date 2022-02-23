3fcc6e4700d3e8a6010b512ff18733b6b60dbc5e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-02-22 20:09:03 -0500

    Dynamically insert keys into keyboard-container

--- app.js
+++ app.js
@@ -2,3 +2,41 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
 const messageDisplay = document.querySelector('.message-container');
+
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '«',
+];
+
+// Add keyboard buttons
+keys.forEach((key) => {
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = key;
+  keyboard.append(buttonElement);
+});
