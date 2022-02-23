c4e80a6810f3c6df4b5307f3d9ba47d642fbb659
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-02-22 21:45:20 -0500

    Setup mini backend and add script

---
+++ index.js
@@ -0,0 +1,8 @@
+const PORT = 8000;
+const axios = require('axios').default;
+const express = require('express');
+const app = express();
+
+app.use();
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
